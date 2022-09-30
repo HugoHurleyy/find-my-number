65fba97f4735d52ef22f24e362977d1a056e781d
HugoHurleyy <59080216+HugoHurleyy@users.noreply.github.com>
2022-10-01 00:07:54 +0300

    added vibration animation

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 'use strict';
-const randomNumber = Math.floor(Math.random()*21);
+const randomNumber = Math.floor(Math.random()*20 + 1);
 let score = 20;
-
+const newspaperSpinning = [
+{ transform: "translate(1px, 1px) rotate(0deg)" },
+{ transform: "translate(-1px, -2px) rotate(-1deg)" },
+{ transform: "translate(-3px, 0px) rotate(1deg)" },
+{ transform: "translate(3px, 2px) rotate(0deg)" },
+{ transform: "translate(1px, -1px) rotate(1deg)" },
+{ transform: "translate(-1px, 2px) rotate(-1deg)" },
+{ transform: "translate(-3px, 1px) rotate(0deg)" },
+{ transform: "translate(3px, 1px) rotate(-1deg)" },
+{ transform: "translate(-1px, -1px) rotate(1deg)" },
+{ transform: "translate(1px, 2px) rotate(0deg)" },
+{ transform: "translate(1px, -2px) rotate(-1deg)" },
+  ];
+  
+const newspaperTiming = {
+    duration: 500,
+    iterations: 1,
+}
+const body = document.querySelector("body");
 
 
 
@@ -11,11 +29,14 @@ function checkAnswer(){
         document.getElementsByTagName("p")[1].textContent = "📉 Too Low";
         score--;
         document.getElementsByTagName("span")[0].textContent = score;
+        body.animate(newspaperSpinning, newspaperTiming);
+        
     }
     else if (Number(inputValue) > randomNumber){
         document.getElementsByTagName("p")[1].textContent = "📈 Too High";
         score--;
         document.getElementsByTagName("span")[0].textContent = score;
+        body.animate(newspaperSpinning, newspaperTiming);
     }
 
     else{
